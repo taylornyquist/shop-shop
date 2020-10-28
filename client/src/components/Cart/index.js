adb5f6e4a4047bf47e54a147c92cf195c90b583d
Taylor Nyquist <tnyquist@gmail.com>
2020-10-28 18:42:30 -0500

    finished indexedDB

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const Cart = () => {
 
     const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -55,12 +67,12 @@ const Cart = () => {
                     </div>
                 </div>
             ) : (
-                <h3>
-                    <span role="img" aria-label="shocked">
-                        😱
+                    <h3>
+                        <span role="img" aria-label="shocked">
+                            😱
                     </span>
                     You haven't added anything to your cart yet!
-                </h3>
+                    </h3>
                 )}
         </div>
     );
